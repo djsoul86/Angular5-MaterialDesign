d23724b2326591b75526b152dbf9f64e4aa9b360
djsoul86 <ernestoandrade86@gmail.com>
2018-03-27 16:18:05 -0500

    primera parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule,MatTableModule } from '@angular/material';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {routes} from './routes';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { LoginComponent } from './public/login/login.component';
+import { HomeComponent } from './auth/home/home.component';
+import { NotFoundComponent } from './common/not-found/not-found.component';
+import { PublicGuard } from './common/guards/public.guard';
+import { AuthGuard } from './common/guards/auth.guard';
+import { AuthenticationService } from './common/services/authentication.service';
+import { Ng2Webstorage } from 'ngx-webstorage';
+import { BookmarksService} from './auth/bookmarks/services/bookmarks.service';
 
+import { TokenInterceptor } from './auth/interceptors/token.interceptor';
+import { BookmarksComponent } from './auth/bookmarks/bookmarks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    NotFoundComponent,
+    BookmarksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    Ng2Webstorage,
+    MatInputModule
   ],
-  providers: [],
+  providers: [PublicGuard,AuthGuard,AuthenticationService,
+    BookmarksService,
+    {provide:HTTP_INTERCEPTORS,useClass:TokenInterceptor,multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
